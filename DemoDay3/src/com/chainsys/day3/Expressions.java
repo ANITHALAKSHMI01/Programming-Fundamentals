31c7ed5e0f8932cb990decfb9e02b5ce17ebab90
anit3573 <anit3573@CSL0688.global.chainsys.com>
2024-04-15 16:56:41 +0530

    Expression

---
+++ DemoDay3/src/com/chainsys/day3/Expressions.java
@@ -0,0 +1,15 @@
+package com.chainsys.day3;
+public class Expressions
+{
+	public static void main(String[] args) 
+	{
+		int x=2;
+		int y=5;
+		int z=0;
+		System.out.println("x==2         -> "+(x==2));
+		System.out.println("x!=5         -> "+(x!=5 ));
+		System.out.println("x!=5 && y>=5 -> "+(x!=5 && y>=5));
+		System.out.println("z!=0 || x==2 -> "+(z!=0 || x==2));
+		System.out.println("!(y<10)      -> "+(!(y<10)));
+	}
+}
