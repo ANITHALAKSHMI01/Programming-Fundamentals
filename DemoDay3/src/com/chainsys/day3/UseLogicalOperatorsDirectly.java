328243e7a726f52e7813b57d566c0f493acff0c8
anit3573 <anit3573@CSL0688.global.chainsys.com>
2024-04-15 17:02:44 +0530

    UseLogical Operators directly

---
+++ DemoDay3/src/com/chainsys/day3/UseLogicalOperatorsDirectly.java
@@ -0,0 +1,12 @@
+package com.chainsys.day3;
+public class UseLogicalOperatorsDirectly 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		System.out.println(a>0 && a<b);
+		System.out.println(b>= 0|| b<=a);
+		System.out.println(a==b);
+	}
+}
