1361fd7da730b6ea6bff438c004b19b3adde5981
anit3573 <anit3573@CSL0688.global.chainsys.com>
2024-04-15 17:08:28 +0530

    Pattern 3

---
+++ DemoDay3/src/com/chainsys/day3/Pattern3.java
@@ -0,0 +1,26 @@
+package com.chainsys.day3;
+public class Pattern3 
+{
+	public static void main(String[] args) 
+	{
+			for(int i=4;i>0;i--)
+			{
+				for(int j=1;j<=5-i;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=(i*2)-1;k++)
+				{
+					if(k%2!=0)
+					{
+						System.out.print("1");
+					}
+					else
+					{
+						System.out.print("0");
+					}
+				}
+				System.out.println();
+			}
+	}
+}
