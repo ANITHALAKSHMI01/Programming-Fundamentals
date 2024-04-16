52ba3f5a2070e0910957923f6e29bc4e7145fbbc
anit3573 <anit3573@CSL0688.global.chainsys.com>
2024-04-16 18:12:16 +0530

    Bank Deposit(Polymorphism)

---
+++ DemoDay3/src/com/chainsys/encapsulation/TestABCBank.java
@@ -0,0 +1,11 @@
+package com.chainsys.encapsulation;
+import java.util.Scanner;
+public class TestABCBank
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		ABCBank bank=new ABCBank();
+		ABCBank.getUserDetails();
+	}
+}
